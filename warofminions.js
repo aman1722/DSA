2b769165e812a5b33e489273dbf0309622b877ab
aman1722 <112754413+aman1722@users.noreply.github.com>
2023-11-02 23:54:17 +0530

    warofminions

---
+++ warofminions.js
@@ -0,0 +1,24 @@
+function fn(n,str){
+    // let n = str.length;
+    let temp =-1;
+    let st=[];
+    for(let i=0;i<n;i++){
+          if(st[temp] == str[i]){
+              st.pop();
+              temp--;
+          }
+          else{
+              st.push(str[i]);
+              temp++;
+          }
+          
+    }
+    if(st.length ===0){
+        console.log("-1");
+    }
+    else{
+        console.log(st.join(""));
+    }
+  
+
+}
